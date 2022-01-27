c6f30a70a6d5dfc8711f52c64048d8b0173083ac
Zahir <zohir.oo1@mail.ru>
2022-01-27 20:05:02 +0500

    Add cocktail function

---
+++ index.php
@@ -0,0 +1,27 @@
+?php
+/*
+Mohito Recipe:
+Laym - ½ piece
+Ice - 6 piece
+Sprite - 150 ml
+Sugar - 10 g 
+Myata - 10 g
+*/
+
+function makeMohito
+(int $Laym,
+ int $Ice,
+ int $sSprite,
+ int $Sugar,
+ int $Myata,
+ {
+	echo 
+	"1.Нарезаем лайм и кладем в бокал <br>
+	 2.Добавляем мяту, тростниковый сахар и разминаем<br>
+	 3.Добавляем ледяную крошку и перекладываем смесь в шейкер. Взбиваем<br>
+	 4.Перекладываем в бокал и заливаем спрайтом<br>
+	 5.Декорируем коктейль листиком мяты и лаймом — напиток готов. <br>
+	 
+	
+ }	 
+ makeMohito(6, 150, 25, 10, 10,'Bon appetit');
